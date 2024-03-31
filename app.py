92d635a259cdf6c440775d1eea644eb3e6abb9d6
Glitch (glitch-hello-website) <none>
2024-03-31 10:16:24 +0000

    🚈☃️ Checkpoint ./templates/home.html:65813575/112 ./app.py:65813575/255 ./templates/contact.html:65813575/106 ./templates/blog.html:65813575/94

--- app.py
+++ app.py
@@ -3,8 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home():
     return render_template('home.html')
+  
+@app.route('/contact')
+def cont_sec():
+    return render_template('contact.html')
+  
+@app.route('/blog')
+def blog_sec():
+    return render_template('blog.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
