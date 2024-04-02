190293fbd249e2e4401e1e45515590f3d70620e0
Glitch (glitch-hello-website) <none>
2024-04-02 05:54:16 +0000

    💺🌽 Checkpoint ./requirements.txt:65813575/28 ./app.py:65813575/814

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join('.data', 'contacts.db')
+db = SQLAlchemy(app)
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    message = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=db.func.now())
+
 
 @app.route('/')
 def home():
@@ -8,6 +20,13 @@ def home():
   
 @app.route('/contact')
 def cont_sec():
+    name = request.form['name']
+    email = request.form['email']
+    message = request.form['message']
+    new_message = Message(name=name, email=email, message=message)
+    db.session.add(new_message)
+    db.session.commit()
+    return 'Message submitted successfully!'
     return render_template('contact.html')
   
 @app.route('/blog')
@@ -15,4 +34,6 @@ def blog_sec():
     return render_template('blog.html')
 
 if __name__ == '__main__':
+    with app.app_context():
+    db.create_all()
     app.run(host='0.0.0.0', port=3000, debug=True)
